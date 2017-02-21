b6663cd2c509a3cd4324e62ce1490537a7895a54
gregce <gregce@dropbox.com>
2017-02-20 16:43:36 -0800

    initial commit

---
+++ initial_setup.R
@@ -0,0 +1,7 @@
+#devtools::install_github('rstudio/blogdown')
+#library(blogdown)
+#setwd("/Users/gregce/gregce_blogdown/")
+#blogdown::new_site()
+#blogdown::install_theme("rakuishi/hugo-zen")
+## confugre toyaml
+
